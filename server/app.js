10324fe7e01c8b943f79ce59f8fa2205ea6b7c3c
Hack Reactor Student <student@hackreactor.com>
2017-11-14 17:38:35 -0800

    use port from env or 3000

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('hello world');
 })
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
