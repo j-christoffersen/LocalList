9f0787da157b79dee7800e62818b5388457aafcc
Brandon Hadler <brandon.hadler30@gmail.com>
2017-11-25 15:54:38 -0600

    nothing changed

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 // expect job to be an object with unique information about that job
+// show if someone claimed it
+// if not, have a button that allows you to claim it
+  // update state of jobs array
+    // array.find(job)
 
 const Job = (props) => {
   return (
