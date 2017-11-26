e5c8373fa3bf949523c25b38408fe5c01ede1bf5
Brandon Hadler <brandon.hadler30@gmail.com>
2017-11-26 16:14:07 -0600

    add home button to NavLink

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,6 +4,7 @@ const NavLink = require('react-router-dom').NavLink;
 const Nav = () => {
   return (
     <div className="nav">
+      <NavLink activeClassName="active" to="/"><button>Home</button></NavLink>
       <NavLink activeClassName="active" to="/login"><button>Log in</button></NavLink>
       <NavLink activeClassName="active" to="/logout"><button>Log out</button></NavLink>
       <NavLink activeClassName="active" to="/signup"><button>Sign up</button></NavLink>
