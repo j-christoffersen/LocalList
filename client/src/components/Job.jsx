d7afa10a66914ccab6cebbe53da1ed8bedc716ee
Jackson Christoffersen <jacksontchristo@gmail.com>
2017-11-26 20:57:43 -0800

    add links to job detail page in job component

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Job = (props) => {
   return (
     <div>
-      <h4>{props.job.name}</h4>
+      <NavLink activeClassName="active" to={`/jobs/${props.job.id}`}>
+        <h4>{props.job.name}</h4>
+      </NavLink>
       <p>{props.job.location}</p>
       <button onClick={ () => {props.onClaimed(props.job)} }>Claim this job!</button>
     </div>
