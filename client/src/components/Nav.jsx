55c350a0b2787aa6d21b8e2196488243769cf0da
Brandon Hadler <brandon.hadler30@gmail.com>
2017-11-27 10:05:59 -0800

    code clean up Nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,18 +1,16 @@
-const React = require('react');
-const NavLink = require('react-router-dom').NavLink;
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Nav = () => {
-  return (
-    <div className="nav">
-      <NavLink activeClassName="active" to="/"><button>Home</button></NavLink>
-      <NavLink activeClassName="active" to="/login"><button>Log in</button></NavLink>
-      <NavLink activeClassName="active" to="/logout"><button>Log out</button></NavLink>
-      <NavLink activeClassName="active" to="/signup"><button>Sign up</button></NavLink>
-      <NavLink activeClassName="active" to="/protected"><button>Members Only!</button></NavLink>
-      <NavLink activeClassName="active" to="/profile"><button>Profile</button></NavLink>
-      <NavLink activeClassName="active" to="/create-job"><button>Create Job</button></NavLink>
-    </div>
-  )
-}
+const Nav = () => (
+  <div className="nav">
+    <NavLink activeClassName="active" to="/"><button>Home</button></NavLink>
+    <NavLink activeClassName="active" to="/login"><button>Log in</button></NavLink>
+    <NavLink activeClassName="active" to="/logout"><button>Log out</button></NavLink>
+    <NavLink activeClassName="active" to="/signup"><button>Sign up</button></NavLink>
+    <NavLink activeClassName="active" to="/protected"><button>Members Only!</button></NavLink>
+    <NavLink activeClassName="active" to="/profile"><button>Profile</button></NavLink>
+    <NavLink activeClassName="active" to="/create-job"><button>Create Job</button></NavLink>
+  </div>
+);
 
-module.exports = Nav;
+export default Nav;
