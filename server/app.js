e98d02a241b0cba40cad9ce571077651f8650ccb
Jackson Christoffersen <jacksontchristo@gmail.com>
2017-11-27 10:09:20 -0800

    clean files, remove test routes and stubs

--- server/app.js
+++ server/app.js
@@ -10,52 +10,50 @@ const router = require('./routes.js');
 
 const app = express();
 
-//passport sessions
-//refactor with promises
-passport.serializeUser(function (user, done) {
+// passport sessions
+// refactor with promises
+passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser(function (userId, done) {
+passport.deserializeUser((userId, done) => {
   models.user.findById(userId)
-  .then(user => {
-    done(null, user);
-  });
+    .then((user) => {
+      done(null, user);
+    });
 });
 
-//refactor to use only promises if possible (remove cb 'done')
-//also maybe move somewhere else
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    models.user.findOne({ where: { username: username }})
-    .then(user => {
+// refactor to use only promises if possible (remove cb 'done')
+// also maybe move somewhere else
+passport.use(new LocalStrategy((username, password, done) => {
+  models.user.findOne({ where: { username } })
+    .then((user) => {
       if (!user) {
         done(null, false, { message: 'Incorrect username.' });
         return;
       }
 
       user.validatePassword(password)
-      .then(passwordIsMatch => {
-        if (passwordIsMatch) {
-          done(null, user);
-        } else {
-          done(null, false, { message: 'Incorrect password.' });
-        }
-      })
+        .then((passwordIsMatch) => {
+          if (passwordIsMatch) {
+            done(null, user);
+          } else {
+            done(null, false, { message: 'Incorrect password.' });
+          }
+        });
     })
-    .catch(err => {
-      done(err); 
+    .catch((err) => {
+      done(err);
     });
-  }
-));
+}));
 
-//middleware
+// middleware
 app.use(express.static(__dirname + '/../client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
   secret: process.env.SESSION_SECRET || 's3cr3t',
-  secure: false
+  secure: false,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -64,9 +62,9 @@ app.use('/api', router);
 
 app.all('/*', (req, res) => {
   res.redirect('/');
-})
+});
 
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const path = require('path');
 const passport = require('passport');
 
 const controllers = require('./controllers');
+
 const JsonHeaders = { 'Content-Type': 'application/json' };
 
-//authorization middleware
+// authorization middleware
 const auth = (req, res, next) => {
   if (req.user) {
     next();
@@ -15,73 +15,61 @@ const auth = (req, res, next) => {
   }
 };
 
-const authStub = (req, res, next) => {
-  req.user = req.user || {id: 2};
-  next();
-}
-
-//test, to remove
-router.route('/test')
-.get((req, res) => {
-  res.writeHead(200);
-  res.end('hey');
-})
-
 router.route('/login')
-.post(passport.authenticate('local'), (req, res) => {
-  let {id, username} = req.user.dataValues;
-  res.writeHead(201, JsonHeaders);
-  res.end(JSON.stringify({id, username}));
-})
+  .post(passport.authenticate('local'), (req, res) => {
+    const { id, username } = req.user.dataValues;
+    res.writeHead(201, JsonHeaders);
+    res.end(JSON.stringify({ id, username }));
+  });
 
 router.route('/signup')
-.post((req, res) => {
-  controllers.user.post(req, res);
-})
+  .post((req, res) => {
+    controllers.user.post(req, res);
+  });
 
 router.route('/auth')
-.get((req, res) => {
-  let user;
-  if (req.user) {
-    user = {
-      id: req.user.id,
-      username: req.user.username
+  .get((req, res) => {
+    let user;
+    if (req.user) {
+      user = {
+        id: req.user.id,
+        username: req.user.username,
+      };
     }
-  }
-  res.writeHead(200, JsonHeaders);
-  res.end(JSON.stringify(user || null));
-})
+    res.writeHead(200, JsonHeaders);
+    res.end(JSON.stringify(user || null));
+  });
 
 
 router.route('/jobs')
-.get(controllers.job.getAll)
-.post(auth, controllers.job.post)
+  .get(controllers.job.getAll)
+  .post(auth, controllers.job.post);
 
 router.route('/jobs/:id')
-.get(controllers.job.getOne)
-.put(auth, controllers.job.markCom)
+  .get(controllers.job.getOne)
+  .put(auth, controllers.job.markCom);
 
 router.route('/jobs/:id/claim')
-.get(controllers.job.claim)
+  .get(controllers.job.claim);
 
 router.route('/reviews')
-.get(controllers.review.get)
-.post(auth, controllers.review.post);
+  .get(controllers.review.get)
+  .post(auth, controllers.review.post);
 
 
 router.route('/logout')
-.get(auth, (req, res) => {
-  req.logout();
-  res.writeHead(204);
-  res.end();
-})
+  .get(auth, (req, res) => {
+    req.logout();
+    res.writeHead(204);
+    res.end();
+  });
 
 
-//test, to remove
+// test, to remove
 router.route('/members')
-.get(auth, (req, res) => {
-	res.writeHead(200);
-	res.end(`Welcome, ${req.user.username}!`);
-})
+  .get(auth, (req, res) => {
+    res.writeHead(200);
+    res.end(`Welcome, ${req.user.username}!`);
+  });
 
 module.exports = router;
